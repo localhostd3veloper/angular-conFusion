f52f49d21447867c8d63d7d290f2d6a0e8145841
localhostd3veloper <gautamanand022@gmail.com>
2022-07-01 12:20:27 +0530

    Selected Dish

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,21 +1,14 @@
+import { Comment } from './comment';
+
 export class Dish {
-  constructor() {
-    this.id = '1';
-    this.name = 'Name Error';
-    this.image = '';
-    this.category = 'category';
-    this.featured = false;
-    this.label = 'ok';
-    this.price = '200$';
-    this.description = 'this will display if nothing comes';
-  }
-  id: string;
-  name: string;
-  image: string;
-  category: string;
-  featured: boolean;
-  label: string;
-  price: string;
-  description: string;
+  id!: string;
+  name!: string;
+  image!: string;
+  category!: string;
+  featured!: boolean;
+  label!: string;
+  price!: string;
+  description!: string;
+  comments!: Comment[];
 }
 [];
