dad489c63565bdd2603fce89dcf909f7fdd1ea00
localhostd3veloper <gautamanand022@gmail.com>
2022-07-01 11:52:57 +0530

    Week 1:Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,10 @@ import 'hammerjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent],
+  declarations: [AppComponent, MenuComponent, DishdetailComponent],
   imports: [
     BrowserModule,
     MatListModule,
